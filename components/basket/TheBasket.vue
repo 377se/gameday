8f0276127debf9cff8c28886d1728f0802d87e72
Henrik Stridsman <henrik@377.se>
2019-10-24 22:11:25 +0200

    Preparing checkout

--- components/basket/TheBasket.vue
+++ components/basket/TheBasket.vue
@@ -17,88 +17,93 @@
           type="button" uk-close/>
       </div>
 
-      <table 
-        v-if="cart.data.length>0"
-        class="uk-table uk-table-justify uk-table-divider uk-margin-remove"
-        style="color:#333333;font-size:0.7rem">
-        <thead
-          style="font-size:0.7rem">
-          <tr v-if="cart.freeShippingMessage">
-            <td colspan="3">
-              <div>{{ cart.freeShippingMessage }}</div>
-            </td>
-          </tr>
-        </thead>
-        <tbody
-          class="uk-background-default">
-          <tr
-            v-for="(item, index) in cart.data"
-            :key="index">
-            <td class="thumb-image">
-              <img 
-                :src="thumb_src + item.ImageThumb" />
-            </td>
-            <td class="article-data">
-              <span class="article-name">
-                {{ item.Category }}<br/>{{ item.Name }}
-                <span
-                  v-if="item.AddOnDisplay!=''"><br />{{ item.AddOnDisplay }}
+      <div
+        v-if="cart.data.length>0">
+        <table 
+          class="uk-table uk-table-justify uk-table-divider uk-margin-remove"
+          style="color:#333333;font-size:0.7rem">
+          <thead
+            style="font-size:0.7rem">
+            <tr v-if="cart.freeShippingMessage">
+              <td colspan="3">
+                <div>{{ cart.freeShippingMessage }}</div>
+              </td>
+            </tr>
+          </thead>
+          <tbody
+            class="uk-background-default">
+            <tr
+              v-for="(item, index) in cart.data"
+              :key="index">
+              <td class="thumb-image">
+                <img 
+                  :src="thumb_src + item.ImageThumb" />
+              </td>
+              <td class="article-data">
+                <span class="article-name">
+                  {{ item.Category }}<br/>{{ item.Name }}
+                  <span
+                    v-if="item.AddOnDisplay!=''"><br />{{ item.AddOnDisplay }}
+                  </span>
                 </span>
-              </span>
-			        <br/>
-              <span
-                v-for="size in item.Size"
-                :key="size.Value" 
-                class="article-size">
-                {{ size.Name }}<br/>
-              </span>
+                <br/>
+                <span
+                  v-for="size in item.Size"
+                  :key="size.Value" 
+                  class="article-size">
+                  {{ size.Name }}<br/>
+                </span>
+              </td>
+              <td 
+                class="total">
+                <div 
+                v-if="item.IsDiscount"
+                style="text-decoration:line-through;">{{ item.PriceDisplay }}</div>
+                <div
+                  v-if="item.PriceAddOn">+{{ PriceAddOnDisplay }}</div>
+                {{item.PriceTotalDisplay}}
+                <br>
+                <a 
+                  href="#"
+                  style="text-decoration:underline;color:#333;"
+                  @click.prevent.stop="deleteFromCart(item.Id)">Ta bort</a>
+              </td>
+            </tr>
+          </tbody>
+          <tfoot
+            class="uk-background-default"
+            style="font-weight:bold;color:#474747;font-size:0.7rem">
+            <!--tr>
+            <td colspan="5" style="text-align:right">
+              {{#if isVoucher}}
+              <form class="form-inline">
+                <div style="margin-bottom:0" class="form-group">
+                  <input type="text" class="form-control" style="" name="VoucherCode" id="VoucherCode" placeholder="{{{dict.voucher-code}}}">
+                </div>
+                <button type="button" class="btn btn-default" can-click="checkVoucher">{{{dict.send}}}</button>
+              </form>
+              {{else}}
+              <a href="#" can-click="showVoucherForm">{{{dict.voucher-code}}}?</a>
+              {{/if}}
             </td>
-            <td 
-              class="total">
-              <div 
-              v-if="item.IsDiscount"
-              style="text-decoration:line-through;">{{ item.PriceDisplay }}</div>
-              <div
-                v-if="item.PriceAddOn">+{{ PriceAddOnDisplay }}</div>
-              {{item.PriceTotalDisplay}}
-              <br>
-              <a 
-                href="#"
-                style="text-decoration:underline;color:#333;"
-                @click.prevent.stop="deleteFromCart(item.Id)">Ta bort</a>
-            </td>
-          </tr>
-        </tbody>
-        <tfoot
-          class="uk-background-default"
-          style="font-weight:bold;color:#474747;font-size:0.7rem">
-          <!--tr>
-          <td colspan="5" style="text-align:right">
-            {{#if isVoucher}}
-            <form class="form-inline">
-              <div style="margin-bottom:0" class="form-group">
-                <input type="text" class="form-control" style="" name="VoucherCode" id="VoucherCode" placeholder="{{{dict.voucher-code}}}">
-              </div>
-              <button type="button" class="btn btn-default" can-click="checkVoucher">{{{dict.send}}}</button>
-            </form>
-            {{else}}
-            <a href="#" can-click="showVoucherForm">{{{dict.voucher-code}}}?</a>
-            {{/if}}
-          </td>
-          </tr-->
-          <tr>
-            <td colspan="2" style="text-align:right">
-              <strong>Totalt <span class="payment-total-ex-shipping">exkl.frakt</span></strong>
-            </td>
-            <td colspan="1" class="total">
-              <strong><span 
-                v-if="cart.IsDiscount"
-                style="text-decoration:line-through;">{{ cart.Price }}<br></span>
-                {{ cart.PriceToPayExShipping }}</strong>
-            </td>
-          </tr>
-        </tfoot>
-      </table>
+            </tr-->
+            <tr>
+              <td colspan="2" style="text-align:right">
+                <strong>Totalt <span class="payment-total-ex-shipping">exkl.frakt</span></strong>
+              </td>
+              <td colspan="1" class="total">
+                <strong><span 
+                  v-if="cart.IsDiscount"
+                  style="text-decoration:line-through;">{{ cart.Price }}<br></span>
+                  {{ cart.PriceToPayExShipping }}</strong>
+              </td>
+            </tr>
+          </tfoot>
+        </table>
+        <nuxt-link
+          to="/checkout"
+          class="uk-button uk-width-1-1 uk-button-default">Till betalning</nuxt-link>
+      </div>
       <div
         v-else 
         class="uk-text-center uk-text-small uk-margin-small">
---
+++ pages/checkout/index.vue
@@ -0,0 +1,83 @@
+<template>
+  <section>
+    <div 
+      id="klarna-checkout"
+      class="uk-container"
+      v-html="klarnahtml"/>
+  </section>
+</template>
+<script>
+export default {
+  head () {
+    return {
+      title: 'Checkout',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Checkout'
+        }
+      ]
+    }
+  },
+  data() {
+    return {
+      klarnahtml: null
+    };
+  },
+  mounted(){
+    var checkoutContainer = document.getElementById('klarna-checkout')
+    var scriptsTags = checkoutContainer.getElementsByTagName('script')
+    // This is necessary otherwise the scripts tags are not going to be evaluated
+    for (var i = 0; i < scriptsTags.length; i++) {
+        var parentNode = scriptsTags[i].parentNode
+        var newScriptTag = document.createElement('script')
+        newScriptTag.type = 'text/javascript'
+        newScriptTag.text = scriptsTags[i].text
+        parentNode.removeChild(scriptsTags[i])
+        parentNode.appendChild(newScriptTag)
+    }
+  },
+  async asyncData({ app, route }) {
+    try {
+      const url = `https://beta.supporterprylar.se/webapi/klarnacheckout3`;
+      const klarnahtml = await app.$axios.$post(url);
+
+      return { klarnahtml };
+    } catch (err) {
+      console.log(err);
+    }
+  }
+}
+/*
+$.ajax({
+                            type: "GET",
+                            url: '/webapi/klarnacheckout3?id=' + _this.scope.id,
+                            contentType: "application/json; charset=utf-8",
+                            dataType: "json",
+                            success: function (data) {
+                                _this.scope.attr('html', true);
+                                _this.scope.attr('ordernumber', data.Ordernumber);
+                                var checkoutContainer = document.getElementById('klarna-checkout')
+                                checkoutContainer.innerHTML = data.Html;
+                                var scriptsTags = checkoutContainer.getElementsByTagName('script')
+                                // This is necessary otherwise the scripts tags are not going to be evaluated
+                                for (var i = 0; i < scriptsTags.length; i++) {
+                                    var parentNode = scriptsTags[i].parentNode
+                                    var newScriptTag = document.createElement('script')
+                                    newScriptTag.type = 'text/javascript'
+                                    newScriptTag.text = scriptsTags[i].text
+                                    parentNode.removeChild(scriptsTags[i])
+                                    parentNode.appendChild(newScriptTag)
+                                }
+                                $('html, body').animate({ scrollTop: 0 }, 100); //Take me to the top again.
+                            },
+                            error: function (data) {
+                                alert('Något gick fel. Kontakta supporten.')
+                            }
+                        });
+*/
+</script>
+<style lang="scss">
+
+</style>
