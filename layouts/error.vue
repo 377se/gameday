18a85c753f9e7110793b5a82f29f8b1f720bce0c
Henrik Stridsman <henrik@377.se>
2020-02-21 15:16:41 +0100

    Error page added

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,10 +1,10 @@
 <template>
-  <div>
-    <component 
-      v-if="story.content.component" 
-      :key="story.content._uid" 
-      :blok="story.content" 
-      :is="story.content.component" />
+  <div class="uk-container uk-padding">
+    <h1>Sidan hittades tyvärr inte :(</h1>
+    <p>Deppa inte över det, vi kan hjälpa dig hitta något kul:</p>
+    <p>
+      <a href="/">Gå till startsidan</a>
+    </p>
   </div>
 </template>
 
@@ -14,62 +14,25 @@ import Page from '@/components/Page'
 export default {
   head () {
     return {
-      title: `${this.story.content.SEO.title}`,
+      title: 'Gameday - 404 - Sidan kan tyvärr inte hittas',
       meta: [
         {
           hid: 'description',
           name: 'description',
-          content: `${this.story.content.SEO.description}`.replace(/<\/?[^>]+(>|$)/g, ""),
+          content: 'Gameday - Sidan hittades inte.',
         },
         {
           hid: 'og:title',
           name:  'og:title',
-          content:  `${this.story.content.SEO.title}`,
+          content:  'Gameday - 404 - Sidan kan tyvärr inte hittas',
         },
         {
           hid: 'og:description',
           name:  'og:description',
-          content: `${this.story.content.SEO.description}`.replace(/<\/?[^>]+(>|$)/g, ""),
+          content: 'Gameday - Sidan hittades inte',
         }
       ]
     }
-  },
-  components: {
-    Page
-  },
-  data () {
-    return {story: { content: {SEO:{title:'',description:''}} }}
-  },
-  mounted(){
-    this.$storybridge.on(['input', 'published', 'change'], (event) => {
-      if (event.action == 'input') {
-        if (event.story.id === this.story.id) {
-          this.story.content = event.story.content
-        }
-      } else {
-        window.location.reload()
-      }
-    })
-    
-  },
-  asyncData (context) {
-    // Check if we are in the editor mode
-    let version = context.query._storyblok || context.isDev ? 'draft' : 'published'
-    // Load the JSON from the API
-    return context.app.$storyapi.get(`cdn/stories/404`, {
-      version: version,
-      cv: context.store.getters.version
-    }).then((res) => {
-      return res.data
-    }).catch((res) => {
-      if (!res.response) {
-        console.error(res)
-        context.error({ statusCode: 404, message: 'Failed to receive content from api' })
-      } else {
-        console.error(res.response.data)
-        context.error({ statusCode: res.response.status, message: res.response.data })
-      }
-    })
   }
 };
 </script>
