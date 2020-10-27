fefa81a185a035f190dfcd94b5916f6704cac886
Mats Barkemo <matsbarkemo@Matss-MBP.Internet>
2020-10-27 15:33:14 +0100

    Storyblok fixes

--- pages/mlb-shop/sale/index.vue
+++ pages/mlb-shop/sale/index.vue
@@ -39,7 +39,7 @@
         <div 
           class="ts-filter uk-flex uk-flex-middle uk-margin-small-bottom"
           uk-sticky="offset:118;width-element:body;bottom:true">
-          <strong>{{ article.TotalNumberOfProducts }} produkter</strong>
+          <strong>{{ article.TotalNumberOfProducts }} {{ $getCMSEntry(global_labels,'info_products', 'produkter') }}</strong>
           <FilterItems
             :product-types="producttypes"
             :colors="colors"
