6f64951c775366638acecab8e1d48cd1da0f4e4d
Henrik Stridsman <henrik@377.se>
2020-11-11 16:11:30 +0100

    Casing

--- plugins/gameday.js
+++ plugins/gameday.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import TheHeader from "@/components/gameday/TheHeader";
 import TheFooter from "@/components/gameday/TheFooter";
-import OffscreenMenu from "@/components/gameday/menu/offscreenMenu";
+import OffscreenMenu from "@/components/gameday/menu/OffscreenMenu";
 import OffscreenProductTypeMenu from "@/components/gameday/menu/ProductTypeMenu";
 import OffscreenBrandMenu from "@/components/gameday/menu/BrandMenu";
 
