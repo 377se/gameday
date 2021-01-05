a1bff2895771be250da0ef95785dfacf0b1b43e8
Tomas Coox <tomas@joox.se>
2021-01-05 11:20:51 +0100

    add component teamicon to supportersplace

--- plugins/supportersplace.js
+++ plugins/supportersplace.js
@@ -4,9 +4,11 @@ import TheFooter from "@/components/supportersplace/TheFooter";
 import OffscreenMenu from "@/components/supportersplace/menu/OffscreenMenu";
 import OffscreenProductTypeMenu from "@/components/supportersplace/menu/ProductTypeMenu";
 import OffscreenBrandMenu from "@/components/supportersplace/menu/BrandMenu";
+import TeamIcon from "@/components/TeamIcon";
 
 Vue.component('TheHeader', TheHeader)
 Vue.component('TheFooter', TheFooter)
 Vue.component('OffscreenMenu', OffscreenMenu)
 Vue.component('OffscreenBrandMenu', OffscreenBrandMenu)
 Vue.component('OffscreenProductTypeMenu', OffscreenProductTypeMenu)
+Vue.component('TeamIcon', TeamIcon)
