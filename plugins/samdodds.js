61faffffb9e842c8e7533cf117cbe3cb36ed9371
Tomas Coox <tomas@joox.se>
2021-01-07 16:05:21 +0100

    update samdodds plugins

--- plugins/samdodds.js
+++ plugins/samdodds.js
@@ -2,8 +2,6 @@ import Vue from 'vue'
 import TheHeader from "@/components/samdodds/TheHeader";
 import TheLFCNews from "@/components/samdodds/TheLFCNews";
 import GameCenterPromo from "@/components/samdodds/gamecenter/GameCenterPromo";
-import NextGame from "@/components/samdodds/gamecenter/NextGame";
-import LastGame from "@/components/samdodds/gamecenter/LastGame";
 import TheFooter from "@/components/samdodds/TheFooter";
 import OffscreenMenu from "@/components/samdodds/menu/OffscreenMenu";
 import OffscreenProductTypeMenu from "@/components/samdodds/menu/ProductTypeMenu";
@@ -12,8 +10,6 @@ import OffscreenBrandMenu from "@/components/samdodds/menu/BrandMenu";
 Vue.component('TheHeader', TheHeader)
 Vue.component('TheLFCNews', TheLFCNews)
 Vue.component('GameCenterPromo', GameCenterPromo)
-Vue.component('NextGame', NextGame)
-Vue.component('LastGame', LastGame)
 Vue.component('TheFooter', TheFooter)
 Vue.component('OffscreenMenu', OffscreenMenu)
 Vue.component('OffscreenBrandMenu', OffscreenBrandMenu)
