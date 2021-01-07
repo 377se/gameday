32ac0b2bd167b4f35d47fc5a1c9b595fc0f25d9a
Tomas Coox <tomas@joox.se>
2021-01-07 12:10:50 +0100

    add /thelab for testing-purposes

---
+++ pages/thelab/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="uk-container uk-container-large uk-padding-small">
+    <NextGame />
+  </div>
+</template>
+<script>
+import NextGame from "@/components/samdodds/gamecenter/NextGame";
+export default {
+  components:{
+    NextGame
+  },
+}
+</script>
