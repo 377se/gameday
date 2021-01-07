c0feecf9298daad2b5f53171f87b1b3863ac07af
Tomas Coox <tomas@joox.se>
2021-01-07 12:09:53 +0100

    add first version of nextgame on samdodds

--- plugins/samdodds.js
+++ plugins/samdodds.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import TheHeader from "@/components/samdodds/TheHeader";
 import TheLFCNews from "@/components/samdodds/TheLFCNews";
+import NextGame from "@/components/samdodds/gamecenter/NextGame";
 import TheFooter from "@/components/samdodds/TheFooter";
 import OffscreenMenu from "@/components/samdodds/menu/OffscreenMenu";
 import OffscreenProductTypeMenu from "@/components/samdodds/menu/ProductTypeMenu";
@@ -8,6 +9,7 @@ import OffscreenBrandMenu from "@/components/samdodds/menu/BrandMenu";
 
 Vue.component('TheHeader', TheHeader)
 Vue.component('TheLFCNews', TheLFCNews)
+Vue.component('NextGame', NextGame)
 Vue.component('TheFooter', TheFooter)
 Vue.component('OffscreenMenu', OffscreenMenu)
 Vue.component('OffscreenBrandMenu', OffscreenBrandMenu)
