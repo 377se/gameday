d7fcb719b1317ee21f3e570f90bfb794e27a6245
Henrik Stridsman <henrik@377.se>
2021-02-05 11:57:42 +0100

    Adding lottie to the global scope

--- plugins/uikit.js
+++ plugins/uikit.js
@@ -1,6 +1,9 @@
 import UIkit from 'uikit'
 import Icons from 'uikit/dist/js/uikit-icons'
 import smoothscroll from 'smoothscroll-polyfill'
+import LottiePlayer from 'lottie-player-vue'
+ 
+Vue.use(LottiePlayer)
 
 // kick off the polyfill!
 smoothscroll.polyfill()
