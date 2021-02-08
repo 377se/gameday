8af53e6c3866e9e7121a1adf72e5b08be5ee0e88
Henrik Stridsman <henrik@377.se>
2021-02-08 19:54:06 +0100

    Handling url for gameday

--- components/promo/ProductsListByCategoryId.vue
+++ components/promo/ProductsListByCategoryId.vue
@@ -6,7 +6,7 @@
       v-for="article in articles"
       :key="article.Id"
       :article="article"
-      :url="`/a/${article.Id}/${article.UrlSafeName}`"
+      :url="(siteid==6)?`/article/${article.HeadCategorySeoName}/${article.SeoName}`:`/a/${article.Id}/${article.UrlSafeName}`"
     />
   </div>
 </template>
