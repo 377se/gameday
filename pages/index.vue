c8ec8458fcef5c06cb80f1e93fab8e6c1752ffc1
Tomas Coox <tomas@joox.se>
2021-02-12 15:32:34 +0100

    remove promosquares gameday

--- pages/index.vue
+++ pages/index.vue
@@ -1,44 +1,6 @@
 <template>
   <section>
     <div class="uk-container uk-container-large uk-padding-small"> 
-      <div 
-        v-if="siteid==6"
-        class="promo-squares uk-text-center uk-flex"
-        style="margin-top:-15px;">
-        <nuxt-link 
-          class="gd-square"
-          :to="localePath('/nhl-shop')">
-          <div>
-            <img src="/logos/nhl_ikon_600x600.png" alt="NHL Shop">
-            <br>Hockey
-          </div>
-        </nuxt-link>
-        <nuxt-link 
-          class="gd-square"
-          :to="localePath('/nfl-shop')">
-          <div>
-            <img src="/logos/nfl_ikon_600x600.png" alt="NFL Shop">
-            <br>Am. Fotboll
-          </div>
-        </nuxt-link>
-        <nuxt-link 
-          class="gd-square"
-          :to="localePath('/nba-shop')">
-          <div>
-            <img src="/logos/nba_ikon_600x600.png" alt="NBA Shop">
-            <br>Basket
-          </div>
-        </nuxt-link>
-        <nuxt-link 
-          class="gd-square"
-          :to="localePath('/mlb-shop')">
-          <div>
-            <img src="/logos/mlb_ikon_600x600.png" alt="MLB Shop">
-            <br>Baseball
-          </div>
-        </nuxt-link>
-      </div>
-
       <component 
         v-if="story.content.component" 
         :key="story.content._uid" 
@@ -120,54 +82,4 @@ export default {
 }
 </script>
 <style lang="scss">
-.promo-squares{
-  background:#002A32;
-  justify-content: center;
-  align-items: center;
-  display: flex;
-  margin-right:-15px;
-  margin-left:-15px;
-  
-}
-.promo-squares a{
-  color: #00CCFF;
-  text-transform:uppercase;
-  white-space:nowrap;
-  font-size:0.4rem;
-  @media (min-width: 480px) { 
-      font-size: 0.7rem;
-  }
-}
-.gd-square{
-  position:relative;
-  width:25%;
-  max-width:100px;
-  padding:10px 0px 10px 8px;
-  justify-content: center;
-  align-items: center;
-  display: flex;
-  background:#002A32;
-  margin:2px;
-  color:#fff;
-  text-decoration: none !important;
-  &:last-child{
-    padding-right:8px;
-  }
-  &:hover, &:active{
-    color:#fff;
-  }
-  & > div > img{
-    width:70px;
-  }
-  & > div > span{
-    margin-top:5px;
-    font-weight:bold;
-    font-size:0.85rem;
-  }
-}
-.gd-square:after{
-  content: '';
-  display: block;
-  padding-bottom: 100%;
-}
 </style>
