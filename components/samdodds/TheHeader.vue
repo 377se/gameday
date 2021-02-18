e8cc8e1bc093ccce31e64d0de766eff76217b0d1
Tomas Coox <tomas@joox.se>
2021-02-18 14:30:17 +0100

    move header-stuff inside container on sammy

--- components/samdodds/TheHeader.vue
+++ components/samdodds/TheHeader.vue
@@ -1,11 +1,11 @@
 <template>
   
-  <div
+  <div style="background-color: #222;"
     uk-sticky>
     <nav 
       class="uk-navbar-container uk-navbar uk-margin header uk-margin-remove-bottom uk-light" 
       uk-navbar>
-      
+    <div class="header-wrapper">
       <div class="uk-navbar-left uk-flex-nowrap">
         <TheHamburger/>
         <nuxt-link 
@@ -88,8 +88,10 @@
           <svg style="min-width:40px;" height='40px' width='40px' fill="#ffffff" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:cc="http://creativecommons.org/ns#" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:svg="http://www.w3.org/2000/svg" xmlns="http://www.w3.org/2000/svg" xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd" xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape" version="1.1" x="0px" y="0px" viewBox="0 0 100 100"><g transform="translate(0,-952.36218)"><path style="text-indent:0;text-transform:none;direction:ltr;block-progression:tb;baseline-shift:baseline;color:#000000;enable-background:accumulate;" d="m 8.9999999,965.36223 c -1.1046,0 -2,0.8954 -2,2 0,1.1046 0.8954,2 2,2 l 12.3750001,0 10.5312,49.74997 c 0.2555,1.2291 1.0765,2.2629 2.0938,2.25 l 50,0 c 1.0566,0.015 2.0312,-0.9433 2.0312,-2 0,-1.0567 -0.9746,-2.015 -2.0312,-2 l -48.375,0 -10.6562,-50.43747 c -0.1983,-0.884 -1.0629,-1.5702 -1.9688,-1.5625 z m 22.0000001,12 6,29.99997 49,0 7,-29.99997 z m 13,47.99997 c -3.8659,0 -7,3.134 -7,7 0,3.866 3.1341,7 7,7 3.866,0 7,-3.134 7,-7 0,-3.866 -3.134,-7 -7,-7 z m 30,0 c -3.8659,0 -7,3.134 -7,7 0,3.866 3.1341,7 7,7 3.866,0 7,-3.134 7,-7 0,-3.866 -3.134,-7 -7,-7 z" fill="#ffffff" fill-opacity="1" stroke="none" marker="none" visibility="visible" display="inline" overflow="visible"></path></g></svg>
         </a>
       </div>
+      </div>
     </nav>
-    <ul 
+    <div class="header-wrapper">
+    <ul
       class="gd-subnav uk-subnav uk-margin-remove-top uk-margin-remove-bottom uk-flex-nowrap uk-margin-remove-left">
       <li
         v-for="cat in menu"
@@ -98,6 +100,7 @@
           :to="localePath('/c/'+(!cat.SubCategoryList.length==0?cat.Id:'0')+'/'+cat.Id+'/'+cat.UrlSafeName)">{{ cat.Name }}</nuxt-link>
       </li>
     </ul>
+  </div>
   </div>
 </template>
 
@@ -163,6 +166,13 @@ export default {
 </script>
 
 <style lang="scss">
+.header-wrapper {
+  position: relative;
+  display: flex;
+  width: 100%;
+  max-width: 1417px;
+  margin: 0 auto;
+}
 .uk-logo{
   padding:0;margin:0 15px;
 }
