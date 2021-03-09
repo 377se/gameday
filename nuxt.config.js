f7ef3ce47137c9ebfcd47a572390d181b76d58d9
Henrik Stridsman <henrik@377.se>
2021-03-09 21:21:28 +0100

    Testar

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,7 +22,7 @@ export default {
     fallback:true
   },
   head: {
-    title: 'Gameday',
+    title: 'Gameday - Static',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
