7f3210e19aa7b103ec2a22ee2d4d45b83edb0461
Tomas Coox <tomas@joox.se>
2021-03-24 13:34:09 +0100

    change localhost URL laxen

--- nuxt.config.laxen.js
+++ nuxt.config.laxen.js
@@ -171,7 +171,7 @@ export default {
   },
   proxy: {
     '/webapi': {
-      target: process.env.NODE_ENV !== 'production'?'https://beta.supporterprylar.se':'https://api.supporterprylar.se'
+      target: process.env.NODE_ENV !== 'production'?'https://beta-api.supporterprylar.se':'https://api.supporterprylar.se'
     }
   },
   /*
