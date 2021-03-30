e9b11c9dc7b0e2d094f855edbb8b9bda68573ecc
Mats Barkemo <matsbarkemo@Matss-MBP.Internet>
2021-03-30 14:46:20 +0200

    Fixar

--- components/articles/ArticleProductTypeList.vue
+++ components/articles/ArticleProductTypeList.vue
@@ -44,7 +44,7 @@
             v-for="article in articles"
             :key="article.ProductId"
             :article="article"
-            :url="(siteid==6)?`/article/${article.HeadCategorySeoName}/${article.SeoName}`:`/a/${article.ProductId}/${article.UrlSafeName}`"
+            :url="(siteid==6)?`/article/${article.TeamNameSeo}/${article.UrlSafeName}`:`/a/${article.ProductId}/${article.UrlSafeName}`"
           />
           <div
             v-if="articles.length<1"
