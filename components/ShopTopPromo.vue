b32440c6fa851622cb3f74e9476c1daaac423116
Tomas Coox <tomas@joox.se>
2021-06-09 10:09:33 +0200

    tweak sizes

--- components/ShopTopPromo.vue
+++ components/ShopTopPromo.vue
@@ -2,13 +2,13 @@
   <div class="uk-width-1-1 stp-container uk-flex uk-flex-center">
     <div class="uk-flex uk-flex-around stp-logo-container">
       <a href="https://suuporterprylar.se">
-        <div class="stp-logo-plate"><img src="/logos/supporterprylar_logo.svg" style="height: 20px;" alt="Logo Supporterprylar"></div>
+        <div class="stp-logo-plate"><img class="stp-logo-image" src="/logos/supporterprylar_logo.svg" alt="Logo Supporterprylar"></div>
       </a>
       <a href="https://samdodds.com">
-        <div class="stp-logo-plate"><img src="/logos/samdodds_logo.svg" style="height: 20px;" alt="Logo Samdodds"></div>
+        <div class="stp-logo-plate"><img class="stp-logo-image" src="/logos/samdodds_logo.svg" alt="Logo Samdodds"></div>
       </a>
       <a href="https://gameday.se">
-        <div class="stp-logo-plate"><img src="/logos/gameday_logo_liggande.svg" style="height: 20px;" alt="Logo Gameday"></div>
+        <div class="stp-logo-plate"><img class="stp-logo-image" src="/logos/gameday_logo_liggande.svg" alt="Logo Gameday"></div>
       </a>
     </div>
   </div>
@@ -26,7 +26,6 @@
   }
   .stp-logo-container {
     margin-top: 6px;
-    // margin-bottom: 3px;
   }
   .stp-logo-plate {
     background-color: #fff;
@@ -34,5 +33,9 @@
     border-top-right-radius: 6px;
     padding: 2px 6px;
     margin: 0 10px;
+    width: 75px;
+  }
+  .stp-logo-image {
+    height: 18px;
   }
 </style>
