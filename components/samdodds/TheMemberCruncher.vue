fd1c3c1a47be43622af82b3c42cc64e23fbdea92
Henrik Stridsman <henrik@377.se>
2021-06-17 14:20:00 +0200

    Test

--- components/samdodds/TheMemberCruncher.vue
+++ components/samdodds/TheMemberCruncher.vue
@@ -71,7 +71,7 @@
                     <strong>1</strong> Medlemsskap (Din leveransadress blir medlemsinformationen)
                 </div>
                 <div>
-                    <strong v-if="currentMembershipObject.FamilyMembers!=null">{{ currentMembershipObject.FamilyMembers.length }}</strong><strong v-else>0</strong> Familjemedlemmar <a href="#familymembers" uk-scroll="offset:120">Lägg till fler</a>
+                    <strong v-if="currentMembershipObject.FamilyMembers!=null">{{ currentMembershipObject.FamilyMembers.length }}</strong><strong v-else>0</strong> Familjemedlemmar <a href="#familymembers" @click.prevent="showAddFamilyMember">Lägg till fler</a>
                 </div>
             </div>
             <div class="uk-width-1-1 uk-width-1-4@s uk-padding-small uk-padding-remove-bottom">
@@ -90,7 +90,13 @@
         <div id="familymembers" class="uk-margin-medium-top uk-width-1-1 uk-width-2-3@m">
             <div>
                 <h3
-                    class="uk-margin-remove-bottom">{{ $getCMSEntry(global_labels, 'membership_family_members', 'Familjemedlemmar') }}</h3>
+                    class="uk-margin-remove-bottom uk-flex uk-flex-middle">{{ $getCMSEntry(global_labels, 'membership_family_members', 'Familjemedlemmar') }} <!-- LÄGG TILL FAMILJEMEDLEM KNAPP -->
+                    <button
+                        class="uk-button uk-button-small uk-button-secondary uk-margin-small-left"
+                        @click.prevent="showAddFamilyMember"
+                        >
+                        {{ $getCMSEntry(global_labels, 'membership_', 'Lägg till familjemedlem') }}
+                    </button></h3>
                 <p class="uk-margin-remove-top">Här kan du lägga till dina familjemedlemmar. En familjemedlem är skriven på samma adress som huvudkontot. Alla familjemedlemmar får ett eget konto hos LFC.se</p>
                 <!-- FAMILJE-LISTA -->
                 <div class="uk-list uk-list-striped">
@@ -166,61 +172,119 @@
                             </div>
                         </div>
                     </div>
+
                 </div>
-                <!-- LÄGG TILL FAMILJEMEDLEM KNAPP -->
-                <div class="uk-margin uk-text-right">
-                    <button
-                        class="uk-button uk-button-small uk-button-secondary uk-margin-small-top"
-                        @click.prevent="getEmptyFamilyMember"
-                        >
-                        {{ $getCMSEntry(global_labels, 'membership_', 'Lägg till familjemedlem') }}
-                    </button>
-                </div>
-                <!-- SÖK MEDLEM -->
-                <div class="uk-margin-small-top uk-margin-medium-bottom">
-                    <h4 style="padding-bottom: 5px;" class="uk-margin-remove">{{ $getCMSEntry(global_labels, 'membership_family_member_search', 'Sök befintlig medlem att lägga till som familjemedlem') }}</h4>
-                        <!-- MEDLEMSNUMMER & POSTNR FÄLT -->
-                        <div class="uk-grid uk-grid-small uk-child-width-expand@s" uk-grid>
+                <div id="modal-family-member" class="uk-modal-container" uk-modal="container:false">
+                    <div class="uk-modal-dialog uk-modal-body">
+                        <button class="uk-modal-close-default" type="button" uk-close></button>
+                        <h2 class="uk-modal-title">Lägg till familjemedlem</h2>
+
+                        <!-- FÄLT FÖR FAMILJEMEDLEM -->
+                        <div class="uk-fieldset uk-grid uk-grid-small uk-grid uk-child-width-expand@s" uk-grid>
                             <div>
-                                <div class="uk-form-controls">
-                                    <input
-                                        v-model="currentFamilyMember.MembershipNumber"
-                                        class="uk-input"
-                                        type="number"
-                                        :placeholder="$getCMSEntry(global_labels, 'membership_number', 'Skriv in medlemsnummer')"
-                                        name="MembershipNumber"
-                                        required>
-                                </div>
-                                <div class="uk-width-1-1 uk-margin-remove">
-                                    <small><a href="javascript:void()" :onclick="'UIkit.modal.alert(\''+$getCMSEntry(global_labels, 'membership_where_to_find_number', '<p>När du vill lägga till en familjemedlem kan du söka fram denna person via medlemsnummer och postnummer om det är så att denne redan finns i supporterklubbens medlemsregister.</p><p>Medlemsnumret hittar ni genom att logga in på LFC.se och gå till “Mina sidor”. Alternativt öppnar ni Cardskipper och tittar på det digitala medlemskortet.</p><p>Ni kan alltid kontakta oss på info@samdodds.com för hjälp kring detta.</p>\'')+')'">{{ $getCMSEntry(global_labels, 'membership_where_to_find_number_label', 'Var hittar jag familjemedlemmens medlemsnummer?') }}</a></small>
-                                </div>
+                                    <div class="uk-inline uk-width-1-1">
+                                        <label class="uk-label">{{ $getCMSEntry(global_labels, 'membership_firstname', 'Skriv in förnamn') }}<span class="form-required">*</span></label>
+                                        <input
+                                            v-model="currentFamilyMember.FirstName"
+                                            class="uk-input"
+                                            type="text"
+                                            :placeholder="$getCMSEntry(global_labels, 'membership_firstname', 'Skriv in förnamn')"
+                                            name="FirstName"
+                                            required
+                                        >
+                                    </div>
                             </div>
                             <div>
-                                <div class="uk-form-controls">
-                                <input
-                                    v-model="currentFamilyMember.PostalCode"
-                                    class="uk-input"
-                                    type="number"
-                                    :placeholder="$getCMSEntry(global_labels, 'membership_postal_code', 'Skriv in postnummer')"
-                                    name="PostalCode"
-                                    required>
-                                </div>
-                            </div>
-                            <!-- KNAPP FÖR SÖK FAMILJEMEDLEM -->
-                            <div>
-                                <button id="search-button" class="uk-button uk-button-secondary uk-align-right" @click.prevent="findFamilyMember()">
-                                    {{ $getCMSEntry(global_labels, 'membership_', 'Sök') }}
-                                </button>
+                                    <div class="uk-inline uk-width-1-1">
+                                        <label class="uk-label">{{ $getCMSEntry(global_labels, 'membership_lastname', 'Skriv in efternamn') }}<span class="form-required">*</span></label>
+                                        <input
+                                            v-model="currentFamilyMember.LastName"
+                                            class="uk-input"
+                                            type="text"
+                                            :placeholder="$getCMSEntry(global_labels, 'membership_lastname', 'Skriv in efternamn')"
+                                            name="LastName"
+                                            required>
+                                        </div>
                             </div>
                         </div>
-                    <!-- ERRORS -->
-                    <Alert
-                        v-if="errors.length > 0"
-                        :errorlist="errors"
-                        message=""
-                        :alertClass="alertClass"
-                    />
+                        <div class="uk-fieldset uk-grid uk-grid-small uk-child-width-expand@s" uk-grid>
+                            <div>
+
+                                <label class="uk-label">{{ $getCMSEntry(global_labels, 'membership_email', 'Skriv in email') }}</label>
+                                <input
+                                    v-model="currentFamilyMember.Email"
+                                    class="uk-input"
+                                    type="text"
+                                    :placeholder="$getCMSEntry(global_labels, 'membership_email', 'Skriv in email')"
+                                    name="Email"
+                                >
+
+                            </div>
+                            <div>
+
+                                <label class="uk-label">{{ $getCMSEntry(global_labels, 'membership_mobile', 'Skriv in mobilnummer') }}</label>
+                                <input
+                                    v-model="currentFamilyMember.Mobile"
+                                    class="uk-input"
+                                    type="text"
+                                    :placeholder="$getCMSEntry(global_labels, 'membership_mobile', 'Skriv in mobilnummer')"
+                                    name="Mobile"
+                                >
+
+                            </div>
+                        </div>
+                        <div class="uk-padding uk-padding-remove-right">
+                            <span class="uk-text-small">(*) Obligatorisk</span> <button class="uk-button uk-button-secondary uk-align-right" @click.prevent="addMemberAsFamilyMember">Spara</button>
+                        </div>
+                        
+                        <!-- SÖK MEDLEM -->
+                        <div class="uk-margin-small-top uk-margin-medium-bottom">
+                            <h4 style="padding-bottom: 5px;" class="uk-margin-remove">{{ $getCMSEntry(global_labels, 'membership_family_member_search', 'Sök befintlig medlem att lägga till som familjemedlem') }}</h4>
+                            <!-- MEDLEMSNUMMER & POSTNR FÄLT -->
+                            <div class="uk-grid uk-grid-small uk-child-width-expand@s" uk-grid>
+                                <div>
+                                    <div class="uk-form-controls">
+                                        <input
+                                            v-model="currentFamilyMember.MembershipNumber"
+                                            class="uk-input"
+                                            type="number"
+                                            :placeholder="$getCMSEntry(global_labels, 'membership_number', 'Skriv in medlemsnummer')"
+                                            name="MembershipNumber"
+                                            required>
+                                    </div>
+                                    <div class="uk-width-1-1 uk-margin-remove">
+                                        <small><a href="javascript:void()" :onclick="'UIkit.modal.alert(\''+$getCMSEntry(global_labels, 'membership_where_to_find_number', '<p>När du vill lägga till en familjemedlem kan du söka fram denna person via medlemsnummer och postnummer om det är så att denne redan finns i supporterklubbens medlemsregister.</p><p>Medlemsnumret hittar ni genom att logga in på LFC.se och gå till “Mina sidor”. Alternativt öppnar ni Cardskipper och tittar på det digitala medlemskortet.</p><p>Ni kan alltid kontakta oss på info@samdodds.com för hjälp kring detta.</p>\'')+')'">{{ $getCMSEntry(global_labels, 'membership_where_to_find_number_label', 'Var hittar jag familjemedlemmens medlemsnummer?') }}</a></small>
+                                    </div>
+                                </div>
+                                <div>
+                                    <div class="uk-form-controls">
+                                    <input
+                                        v-model="currentFamilyMember.PostalCode"
+                                        class="uk-input"
+                                        type="number"
+                                        :placeholder="$getCMSEntry(global_labels, 'membership_postal_code', 'Skriv in postnummer')"
+                                        name="PostalCode"
+                                        required>
+                                    </div>
+                                </div>
+                                <!-- KNAPP FÖR SÖK FAMILJEMEDLEM -->
+                                <div class="uk-width-auto">
+                                    <button id="search-button" class="uk-button uk-button-default uk-align-right" @click.prevent="findFamilyMember()">
+                                        {{ $getCMSEntry(global_labels, 'membership_', 'Sök') }}
+                                    </button>
+                                </div>
+                            </div>
+                            <!-- ERRORS -->
+                            <Alert
+                                v-if="errors.length > 0"
+                                :errorlist="errors"
+                                message=""
+                                :alertClass="alertClass"
+                            />
+                        </div>
+                    </div>
                 </div>
+                    
             </div>
         </div>
     </div>
@@ -255,7 +319,7 @@ export default {
             emptyMembershipObject: [],
             currentMembershipObject: [],
             emptyFamilyMember: [],
-            currentFamilyMember: {MembershipNumber:'',PostalCode:''},
+            currentFamilyMember: {MembershipNumber:'',PostalCode:'', Mobile: '', Email:'', LastName:'', FirstName:''},
             errors: [],
             message: '',
             alertClass: '',
@@ -264,32 +328,40 @@ export default {
         }
     },
     methods: {
-        async getEmptyFamilyMember() {
-            var _this = this;
-            _this.errors = []
-            _this.isSubmitting = true
-            await this.$axios.get('/webapi/'+this.$i18n.locale+'/BranchSe/GetEmptyFamilyMember', _this.currentMembershipObject.FamilyMembers)
-            .then(function (res) {
-                _this.isSubmitting = false
-                try{
-                    if (res.data.ErrorList!=null && res.data.ErrorList.length > 0) {
-                        _this.message = ''
-                        _this.alertClass = 'uk-alert-danger'
-                        _this.errors = res.data.ErrorList
-                    } else {
-                        _this.errors = []
-                        _this.alertClass = 'uk-alert-success'
-                        _this.message = res.data.Message
-                        _this.emptyFamilyMember = res.data
-                        _this.currentFamilyMember = res.data
-                        _this.addMemberAsFamilyMember()
-                    }
-                } catch(err) { console.log(err) }
-            })
-            .catch(function (error) {
-                _this.isSubmitting = false
-            console.log(error)
-            })
+        showAddFamilyMember(){
+            UIkit.modal('#modal-family-member').show()
+        },
+        async addMemberAsFamilyMember() {
+            if(this.currentFamilyMember.FirstName.trim()=='' || this.currentFamilyMember.LastName.trim()==''){
+                UIkit.modal.alert('Du måste fylla i för- och efternamn!', {stack:true})
+            }else{
+                var _this = this;
+                _this.errors = []
+                _this.isSubmitting = true
+                await this.$axios.get('/webapi/'+this.$i18n.locale+'/BranchSe/GetEmptyFamilyMember', _this.currentMembershipObject.FamilyMembers)
+                .then(function (res) {
+                    _this.isSubmitting = false
+                    try{
+                        if (res.data.ErrorList!=null && res.data.ErrorList.length > 0) {
+                            _this.message = ''
+                            _this.alertClass = 'uk-alert-danger'
+                            _this.errors = res.data.ErrorList
+                        } else {
+                            _this.errors = []
+                            _this.alertClass = 'uk-alert-success'
+                            _this.message = res.data.Message
+                            _this.emptyFamilyMember = res.data
+                            _this.currentMembershipObject.FamilyMembers.push({...res.data, ..._this.currentFamilyMember })
+                            _this.currentFamilyMember = {MembershipNumber:'',PostalCode:'', Mobile: '', Email:'', LastName:'', FirstName:''}
+                            UIkit.modal('#modal-family-member').hide()
+                        }
+                    } catch(err) { console.log(err) }
+                })
+                .catch(function (error) {
+                    _this.isSubmitting = false
+                console.log(error)
+                })
+            }
         },
         async findFamilyMember() {
             var _this = this;
@@ -317,9 +389,6 @@ export default {
                 _this.isSubmitting = false
             console.log(error)
             })
-        },
-        addMemberAsFamilyMember() {
-            this.currentMembershipObject.FamilyMembers.push(this.currentFamilyMember)
         },
         addMembership(){
             var _this = this
