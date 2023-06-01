391075e06ff72078b1dec0a90fe73b4750861183
Henrik Stridsman <henrik@377.se>
2023-06-01 12:44:51 +0200

    Api

--- nuxt.config.samdodds.se.js
+++ nuxt.config.samdodds.se.js
@@ -197,7 +197,7 @@ export default {
     '/webapi': {
       target: process.env.NODE_ENV !== 'production'?'https://api.samdodds.com':'https://api.samdodds.com',
       secure: false,
-      changeOrigin: true
+      changeOrigin: false
     }
   },
   /*
