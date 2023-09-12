4f14d05b52f71c9216cbf88d098256666a25c0f3
Henrik Stridsman <henrik@377.se>
2023-09-12 07:23:28 +0200

    SP menu

--- components/supporterprylar/menu/OffscreenMenu.vue
+++ components/supporterprylar/menu/OffscreenMenu.vue
@@ -90,7 +90,7 @@ export default {
   async fetch() {
     try {
       let [menu] = await Promise.all([
-          this.$axios.$get('/webapi/'+this.$i18n.locale+'/category')
+          this.$axios.$get('/webapi/'+this.$i18n.locale+'/category/get')
       ]);
       this.menu = menu
     } catch (err) {
