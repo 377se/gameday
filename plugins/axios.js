7eab44a6af84cdb3d2b9c1fcd244bf4a3f3551e4
Henrik Stridsman <henrik@377.se>
2023-10-05 16:15:27 +0200

    axios

--- plugins/axios.js
+++ plugins/axios.js
@@ -3,15 +3,17 @@
 
 export default function ({ $axios, redirect, app }) {
   $axios.onRequest(config => {
-    config.headers.common['x-shop'] = process.env.SITE_ID
+    config.headers['x-Shop'] = process.env.SITE_ID
     let xculture = app.i18n.locale?app.i18n.locale:app.i18n.defaultLocale
     if(xculture=='en'){xculture='en-gb'}
-    config.headers.common['x-culture'] = xculture
-    if(app.$cookies.get('session')!=undefined){
-      config.headers.common['x-session'] = app.$cookies.get('session')
-    } 
-    if(app.$cookies.get('voucherid')!=undefined && app.$cookies.get('voucherid')!=null){
-      config.headers.common['x-voucherid'] = app.$cookies.get('voucherid')
+    config.headers['x-culture'] = xculture
+    if (app.$cookies) {
+      if (app.$cookies.get('session') != undefined) {
+        config.headers['x-session'] = app.$cookies.get('session')
+      }
+      if (app.$cookies.get('voucherid') != undefined && app.$cookies.get('voucherid') != null) {
+        config.headers['x-voucherid'] = app.$cookies.get('voucherid')
+      }
     }
   })
   }
