1fa3733e52bbbf540d688b56e867be7b405f0f6d
Henrik Stridsman <henrik@bonmoja.com>
2024-09-09 14:28:50 +0200

    Fix.

--- pages/c/_parentid/_categoryid/_categoryname/index.vue
+++ pages/c/_parentid/_categoryid/_categoryname/index.vue
@@ -79,9 +79,8 @@ export default {
     let version = this.$route.query._storyblok || this.$nuxt.context.isDev ? 'draft' : 'published'
     try {
       const [sb] = await Promise.all([
-        this.$storyapi.get('cdn/stories?starts_with=' + process.env.STORYBLOK_CATALOGUE.replace('/','') + '/' +this.$i18n.locale+ '/c/'+this.$route.params.parentid+'/'+this.$route.params.categoryid, {
+        this.$storyapi.get('cdn/stories?sortby=slug:asc&starts_with=' + process.env.STORYBLOK_CATALOGUE.replace('/','') + '/' +this.$i18n.locale+ '/c/'+this.$route.params.parentid+'/'+this.$route.params.categoryid, {
           version: version,
-          sortby: 'slug:desc',
           cv: this.$store.getters.version
         })
       ]);
