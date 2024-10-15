ce882f7bc56b9ab203df6b7ab0334f82026dd971
Henrik Stridsman <henrik@bonmoja.com>
2024-10-15 13:48:06 +0200

    Callback to send code to BE

--- pages/callback/cognito/login.vue
+++ pages/callback/cognito/login.vue
@@ -1,7 +1,11 @@
 <template>
-  <div>{{ this.$auth.user }}</div>
+  <div>Sending code to backend-api</div>
 
 </template>
 <script>
-
+export default{
+  async fetch(){
+    this.$axios.get('/webapi/kopshop/CreateToken?code='+this.$route.query.code)
+  }
+}
 </script>
