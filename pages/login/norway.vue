2f19d54c580b854af4c5888fb6760857cfe6424d
Henrik Stridsman <henrik@bonmoja.com>
2024-10-15 15:59:45 +0200

    New url to login via norway

--- pages/login/norway.vue
+++ pages/login/norway.vue
@@ -4,7 +4,7 @@
     <div
       v-else>
       <p>You are not logged in. </p>
-      <button class="uk-button" @click="login">Login</button>
+      <a href="https://auth.a.liverpool.no/login?client_id=65qi5lmu89hgcr4ma1aipaleus&response_type=code&scope=email+openid+phone+profile&redirect_uri=https%3A%2F%2Fkopshop.no%2Fcallback%2Fcognito%2Flogin" class="uk-button">Login</a>
     </div>
   </div>
 </template>
@@ -16,9 +16,7 @@ export default {
       loggedIn: false
     };
   },
-  created() {
-    this.loggedIn = this.$auth.strategy.token.get();
-  },
+  
   methods: {
     login() {
       this.$auth.loginWith("awsCognito");
