31f69681b73881714b45700e66a2c06c479f82fa
Henrik Stridsman <henrik@bonmoja.com>
2024-10-16 20:09:52 +0200

    TOken.

--- pages/callback/cognito/login.vue
+++ pages/callback/cognito/login.vue
@@ -1,11 +1,14 @@
 <template>
   <div>Sending code to backend-api</div>
+  <div>{{this.$route.query.code}}</div>
+  <div>{{ token }}</div>
 
 </template>
 <script>
 export default{
   async fetch(){
-    this.$axios.get('/webapi/kopshop/CreateToken?code='+this.$route.query.code)
+    const token = await this.$axios.get('/webapi/kopshop/CreateToken?code='+this.$route.query.code)
+    
   }
 }
 </script>
