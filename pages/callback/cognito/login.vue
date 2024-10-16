8e10b200ee4e59ebe59127c451ad4302964c502e
Henrik Stridsman <henrik@bonmoja.com>
2024-10-16 20:52:41 +0200

    nu

--- pages/callback/cognito/login.vue
+++ pages/callback/cognito/login.vue
@@ -3,13 +3,22 @@
     <div>Sending code to backend-api</div>
     <div>Query {{$route.query.code}}</div>
     <div>Token {{ token }}</div>
+    <div><button type="button" @click="sendToNorway()">Skicka</button></div>
   </div>
 </template>
 <script>
 
-export default{
+export default {
   async fetch(){
     const token = await this.$axios.get('/webapi/kopshop/CreateToken?code='+this.$route.query.code) 
+  },
+  mounted(){
+    alert('KUKEN')
+  },
+  methods:{
+    async sendToNorway(){
+      const token = await this.$axios.get('/webapi/kopshop/CreateToken?code='+this.$route.query.code) 
+    }
   }
 };
 
