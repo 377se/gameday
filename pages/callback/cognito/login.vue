b3e164872cc9889cec515ee8b3ffca3346a43570
Henrik Stridsman <henrik@bonmoja.com>
2024-10-16 20:43:45 +0200

    More new

--- pages/callback/cognito/login.vue
+++ pages/callback/cognito/login.vue
@@ -1,14 +1,16 @@
 <template>
-  <div>Sending code to backend-api</div>
-  <div>Query {{$route.query.code}}</div>
-  <div>Token {{ token }}</div>
-
+  <div>
+    <div>Sending code to backend-api</div>
+    <div>Query {{$route.query.code}}</div>
+    <div>Token {{ token }}</div>
+  </div>
 </template>
 <script>
+
 export default{
   async fetch(){
-    const token = await this.$axios.get('/webapi/kopshop/CreateToken?code='+this.$route.query.code)
-    
+    const token = await this.$axios.get('/webapi/kopshop/CreateToken?code='+this.$route.query.code) 
   }
-}
+};
+
 </script>
