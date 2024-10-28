232928531d62ae1808a810c53fc7eb9f5556f129
Henrik Stridsman <henrik@bonmoja.com>
2024-10-28 09:43:46 +0100

    Login-object changed.

--- pages/callback/cognito/login.vue
+++ pages/callback/cognito/login.vue
@@ -18,11 +18,11 @@ export default {
     async sendToNorway(){
       var _this = this
       await this.$axios.post('/webapi/kopshop/CreateToken?code='+this.$route.query.code).then(function (response) {
-          if(response.data.ErrorList.length>0){
-            _this.errors = response.data.ErrorList
+          if(!response.IsLoggedIn){
+            _this.errors = response.ErrorList
           }else{
-            _this.$store.commit('setCid', response.data.Id)
-            _this.$cookies.set('cid', response.data.Id)
+            _this.$store.commit('setCid', response.Id)
+            _this.$cookies.set('cid', response.Id)
             _this.$router.push(_this.localePath('/')) 
           }
       })
