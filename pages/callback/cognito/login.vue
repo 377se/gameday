fa83f7f678fb4b78edc22819141eb2c151f62994
Henrik Stridsman <henrik@bonmoja.com>
2024-10-28 10:20:48 +0100

    Fix.

--- pages/callback/cognito/login.vue
+++ pages/callback/cognito/login.vue
@@ -8,7 +8,7 @@
 export default {
   data(){
     return{
-      error: null
+      errors: null
     }
   },
   async mounted(){
@@ -19,15 +19,16 @@ export default {
       var _this = this
       await this.$axios.post('/webapi/kopshop/CreateToken?code='+this.$route.query.code).then(function (response) {
           if(!response.IsLoggedIn){
-            _this.errors = response.ErrorList
+            _this.errors = response
           }else{
+            _this.errors = response
             _this.$store.commit('setCid', response.Id)
             _this.$cookies.set('cid', response.Id)
             _this.$router.push(_this.localePath('/')) 
           }
       })
       .catch(function (error) {
-        _this.error = error
+        _this.errors = error
         console.log(error)
       })
     
